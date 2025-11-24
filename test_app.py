5a6c8723bab884f7666e1b809d2cc017994fb29f
s.thatavarthi403 <sailiash.learnings403@gmail.com>
2025-11-24 17:29:20 +0530

    added test cases to the pipeline

---
+++ test_app.py
@@ -0,0 +1,17 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.testing = True
+    return app.test_client()
+
+def test_home_status_code(client):
+    """Test if the home route returns HTTP 200."""
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_home_content(client):
+    """Test if the home route returns the correct message."""
+    response = client.get("/")
+    assert response.data.decode("utf-8") == "Hello from Flask CI/CD Pipeline!!!!!!"
